57c18148f88de35356b323ebd54cdf0b21003c53
liwenjie119 <liwenjie119@126.com>
2025-01-24 00:32:05 +0800

    fix diy.sh

--- .github/diy.sh
+++ .github/diy.sh
@@ -12,41 +12,25 @@ function merge_package(){
 }
 rm -rf package/myapp; mkdir package/myapp
 
-
-git clone https://github.com/kongfl888/luci-app-adguardhome package/myapp/luci-app-adguardhome
 git clone https://github.com/2512500960/zzu-minieap-openwrt package/myapp/zzu-minieap-openwrt
-#git clone https://github.com/tty228/luci-app-serverchan package/myapp/luci-app-serverchan
-git clone https://github.com/yaof-project/luci-app-ikoolproxy package/myapp/luci-app-ikoolproxy
+git clone https://github.com/tty228/luci-app-wechatpush package/myapp/luci-app-wechatpush
+git clone https://github.com/yaof2/luci-app-ikoolproxy package/myapp/luci-app-ikoolproxy
 git clone https://github.com/destan19/OpenAppFilter.git package/myapp/OpenAppFilter
-#rm -rf package/lean/luci-app-cpufreq
-#tar zxf ../.github/luci-app-cpufreq-modified.tar.gz -C package/lean
-#git clone https://github.com/tuanqing/install-program package/myapp/install-program
 # helloworld&&lienol
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld;main' feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-server package/myapp/luci-app-ssr-mudb-server
-merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-ssr-mudb-server
-#svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/myapp/aliyundrive-webdav
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
 #晶晨宝盒
-#svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 merge_package https://github.com/ophub/luci-app-amlogic luci-app-amlogic/luci-app-amlogic
 sed -i "s|https.*/OpenWrt|https://github.com/liwenjie119/openwrt_packit|g" package/myapp/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|arm|g" package/myapp/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|opt/kernel|https://github.com/breakings/OpenWrt/tree/main/opt/kernel|g" package/myapp/luci-app-amlogic/root/etc/config/amlogic
 
-# replace antfs 
-sed -i 's/antfs-mount/ntfs-3g/g' ./package/lean/automount/Makefile
 
-#luci-theme-argon
-rm -rf package/lean/luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/myapp/luci-app-argon-config
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-#sed -i '34 s/$/ +libselinux +libsepol/' feeds/packages/utils/lvm2/Makefile
-#sed -i '34 s/$/ +libselinux/' feeds/packages/utils/vim/Makefile
-#sed -i 's/luci-lib-ipkg/luci-base/g' package/feeds/helloworld/luci-app-ssr-plus/Makefile
 sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' target/linux/armvirt/Makefile
 
 #临时修改修复
