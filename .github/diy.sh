a2a2a293ec786154c8a4b1c09e73036b1b2f5195
liwenjie119 <liwenjie119@126.com>
2025-01-26 22:41:48 +0800

    临时替换ssr-libeb

--- .github/diy.sh
+++ .github/diy.sh
@@ -33,6 +33,12 @@ sed -i "s|opt/kernel|https://github.com/breakings/OpenWrt/tree/main/opt/kernel|g
 ./scripts/feeds install -a
 sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' target/linux/armvirt/Makefile
 
+#临时替换shadowsocksr-libev
+rm -rf feeds/helloworld/shadowsocksr-libev
+git clone https://github.com/sbwml/openwrt_helloworld
+mv openwrt_helloworld/shadowsocksr-libev feeds/helloworld/shadowsocksr-libev
+rm -rf openwrt_helloworld
+
 #临时修改修复
 sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
 sed -i "70 s/$/ -D_LARGEFILE64_SOURCE/" feeds/packages/utils/xfsprogs/Makefile
